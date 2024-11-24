ca2d58f583e72697a422f1cd3aec45a4a773aa2b
Duong <anhduong29082003@gmail.com>
2024-11-24 11:26:57 +0700

    create DBwarehouse

---
+++ datawarehouseDBwarehouse/src/main/java/com/example/datawarehouseDBwarehouse/DatawarehouseDBwarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.example.datawarehouseDBwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatawarehouseDBwarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatawarehouseDBwarehouseApplication.class, args);
+	}
+
+}
---
+++ datawarehouseDBwarehouse/src/test/java/com/example/datawarehouseDBwarehouse/DatawarehouseDBwarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datawarehouseDBwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatawarehouseDBwarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
