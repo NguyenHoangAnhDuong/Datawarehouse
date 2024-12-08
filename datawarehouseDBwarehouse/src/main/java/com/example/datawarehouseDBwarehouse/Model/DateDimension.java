5f163f12a9486f65627097560fa8ced09520b976
Lê Quốc Trung <115451384+Danny2611@users.noreply.github.com>
2024-12-08 16:57:50 +0700

    create Model

---
+++ datawarehouseDBwarehouse/src/main/java/com/example/datawarehouseDBwarehouse/Model/DateDimension.java
@@ -0,0 +1,11 @@
+package com.example.datawarehouseDBwarehouse.model;
+
+public class DateDimension {
+    private int id;
+    private String fullDate;
+    private String dayOfWeek;
+    private String month;
+    private String year;
+
+    // Getter và Setter
+}
---
+++ datawarehouseDBwarehouse/src/main/java/com/example/datawarehouseDBwarehouse/Model/SourceDimension.java
@@ -0,0 +1,9 @@
+package com.example.datawarehouseDBwarehouse.model;
+
+public class SourceDimension {
+    private int id;
+    private String sourceName;
+    private String sourceUrl;
+
+    // Getter và Setter
+}
